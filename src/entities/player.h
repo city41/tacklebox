50a65d6868ee8d8f17cbe741b46ac94c23ae7214
Matt Greer <matt.e.greer@gmail.com>
2018-11-02 18:46:37 -0700

    Combined player with ardynia entity, to make way for ardukoi entity

--- src/entities/player.h
+++ src/entities/player.h
@@ -1,16 +1,25 @@
-#ifndef player_h
-#define player_h
+#pragma once
 
+#include <Arduboy2.h>
 #include <Arduino.h>
-#include "entity.h"
+#include "direction.h"
 #include "../maskBitmaps.h"
 
-class Player: public Entity {
+class Player {
     public:
+        int16_t x;
+        int16_t y;
+        int16_t prevX;
+        int16_t prevY;
+        Direction dir;
         bool movedThisFrame;
 
         Player(int16_t px, int16_t py):
-            Entity(),
+            x(0),
+            y(0),
+            prevX(0),
+            prevY(0),
+            dir(DOWN),
             movedThisFrame(false)
         {
             moveTo(px, py);
@@ -19,7 +28,43 @@ class Player: public Entity {
         void render(uint8_t frame);
         void update(uint8_t frame);
         bool isOnSolidTile(void);
-};
 
-#endif
+        Direction determineDirection(int16_t px, int16_t py, int16_t x, int16_t y, Direction prevDir) {
+            if (px == x && py == y) {
+                return prevDir;
+            }
+
+            if (px == x) {
+                if (py > y) {
+                    return UP;
+                }
+                return DOWN;
+            } else {
+                if (px > x) {
+                    return LEFT;
+                }
+                return RIGHT;
+            }
+        }
+
+        inline void moveTo(int16_t newX, int16_t newY, boolean resetPrev = false) {
+            if (resetPrev) {
+                prevX = newX;
+                prevY = newY;
+            } else if (prevX != x || prevY != y) {
+                prevX = x;
+                prevY = y;
+            }
+
+            x = newX;
+            y = newY;
+
+            dir = determineDirection(prevX, prevY, x, y, dir);
+        }
+
+        inline void undoMove() {
+            x = prevX;
+            y = prevY;
+        }
+};
 
