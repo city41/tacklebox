6cad9b35e34b8120d43d2957d5f9dd5465b32ec9
Matt Greer <matt.e.greer@gmail.com>
2018-11-21 19:13:58 -0800

    worm: duration and more common at night

--- src/worm.cpp
+++ src/worm.cpp
@@ -7,12 +7,29 @@ extern Renderer renderer;
 extern Arduboy2Base arduboy;
 
 void Worm::update(uint8_t frame, bool isActive, bool justBecameActive) {
-    isSpawned = isActive && (isSpawned || (justBecameActive && random(0, 3) == 0));
+
+    if (justBecameActive && !isSpawned) {
+        const uint8_t randomRange = State::isDay() ? 8 : 2;
+
+        isSpawned = random(0, randomRange) == 0;
+
+        if (isSpawned) {
+            duration = random(4 * 60, 10 * 60);
+        }
+    }
+
+    if (isSpawned) {
+        duration -= 1;
+
+        justWentInGround = duration > 0 && duration < 14;
+        isSpawned = duration > 0;
+    }
 }
 
 void Worm::render(uint8_t frame) {
     if (isSpawned) {
-         renderer.drawPlusMask(x, y, worm_plus_mask, 0, static_cast<MirrorMode>(frame > 50), Xor);
+        uint8_t index = justWentInGround ? 1 : 0;
+        renderer.drawPlusMask(x, y, worm_plus_mask, index, static_cast<MirrorMode>(frame > 50), Xor);
     }
 }
 
--- src/worm.h
+++ src/worm.h
@@ -7,11 +7,15 @@ class Worm {
         bool isSpawned;
         int16_t x;
         int16_t y;
+        uint16_t duration;
+        bool justWentInGround;
 
         Worm(int16_t x, int16_t y):
             isSpawned(false),
             x(x),
-            y(y)
+            y(y),
+            duration(0),
+            justWentInGround(false)
         {}
 
     void update(uint8_t frame, bool isActive, bool justBecameActive);
