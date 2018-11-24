41a18630a2aedbff78e03086936487fe1abe5c4e
Matt Greer <matt.e.greer@gmail.com>
2018-11-24 15:53:49 -0800

    much better map tiles

--- src/worms.h
+++ src/worms.h
@@ -1,18 +1,16 @@
 #pragma once
 
-const uint8_t MAX_WORMS = 12;
+const uint8_t MAX_WORMS = 10;
 
 Worm worms[MAX_WORMS] = {
 	Worm(309, 38),
 	Worm(69, 103),
-	Worm(389, 341),
+	Worm(389, 292),
 	Worm(38, 141),
-	Worm(55, 154),
-	Worm(57, 201),
+	Worm(516, 103),
+	Worm(500, 339),
 	Worm(247, 326),
 	Worm(245, 102),
-	Worm(168, 70),
-	Worm(42, 328),
 	Worm(72, 345),
 	Worm(372, 184)
 };
