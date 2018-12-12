4481831879a03d93f1adfe20f7c020041de71c89
Matt Greer <matt.e.greer@gmail.com>
2018-12-11 17:41:40 -0800

    Remove web/public

--- web/public/component---src-pages-404-js-76d6014541a2e6dfd959.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{143:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"not found :("),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-76d6014541a2e6dfd959.js.map
